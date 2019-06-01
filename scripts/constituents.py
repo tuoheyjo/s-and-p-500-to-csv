c9e72ca189c01c5ef3922dfd192f9dfb0d7db06e
flynneva <evanflynn.msu@gmail.com>
2019-06-01 11:11:48 -0700

    fixed python script issues

--- scripts/constituents.py
+++ scripts/constituents.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python
 from bs4 import BeautifulSoup
 import csv
 from os import mkdir
 from os.path import exists, join
-import urllib.request as request
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
 
 
 datadir = join('..', 'data')
+
 if not exists(datadir):
     mkdir(datadir)
 
@@ -16,7 +21,11 @@ source = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 cache = join('tmp', 'List_of_S%26P_500_companies.html')
 
 def retrieve():
-    request.urlretrieve(source, cache)
+    html = urlopen(source)
+    page_content = html.read()
+    with open(cache, 'w') as fid:
+        fid.write(page_content)
+
 
 def extract():
     source_page = open(cache).read()
@@ -25,7 +34,10 @@ def extract():
 
     # Fail now if we haven't found the right table
     header = table.findAll('th')
-    if header[0].string != "Ticker symbol" or header[1].string != "Security":
+    #print(header[0].string, 'is', bool(header[0].string != None))
+    #print(header[1].string, 'is', bool(header[1].string != "Security"))
+
+    if header[0].string != None or header[1].string != "Security":
         raise Exception("Can't parse wikipedia's table!")
 
     # Retreive the values in the table
@@ -48,8 +60,11 @@ def extract():
     writer.writerows(records)    
 
 def process():
+    print("inside process function")
     retrieve()
+    print("ran retrieve function")
     extract()
+    print("ran extract function")
 
 if __name__ == '__main__':
     process()
