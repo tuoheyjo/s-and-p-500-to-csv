8d361060bede1e8289c5ff50ba5d8192b072203d
Travis CI <travis@travis-ci.org>
2019-06-01 17:05:18 -0700

    Travis build:

---
+++ scripts/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+init_git() {
+	git config --global user.email "travis@travis-ci.org"
+	git config --global user.name "Travis CI"
+}
+
+commit_files() {
+	git checkout -b gh-pages
+	git add . *.html
+	git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+	git remote add origin-pages https://${GITHUB_UPDATE_TOKEN}@github.com/flynneva/db_stock_functions.git > /dev/null 2>&1
+	git push --quiet --set-upstream
+}
+
+init_git
+commit_files
+upload_files
